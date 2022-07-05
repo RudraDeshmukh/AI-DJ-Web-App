a5c943bfa2e2171ccda5029ed1cb9338770a97da
RudraDeshmukh <99274112+RudraDeshmukh@users.noreply.github.com>
2022-07-05 20:46:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ leftwristx=0;
 leftwristy=0;
 rightwristx=0;
 rightwristy=0;
+scorerightwrist=0;
+scoreleftwrist=0;
 
 function preload(){
     song = loadSound("music.mp3");
@@ -25,6 +27,39 @@ function modelloaded(){
 
 function draw(){
     image(video,0,0,600,500);
+    fill("#FF0000")
+    stroke("FF0000")
+    if(scoreleftwrist > 0.2){
+        circle(leftwristx,leftwristy,20)
+        Innumber=Number(leftwristy)
+        remove_decimal=floor(Innumber)
+        volume=remove_decimal/500
+        song.setVolume(volume);
+        document.getElementById("volume").innerHTML="volume = "+ volume;
+    }
+    if(scorerightwrist > 0.2){
+        circle(rightwristx,rightwristy,20)
+        if(rightwristy > 0 && rightwristy <= 100){
+            song.rate(0.5);
+            document.getElementById("speed").innerHTML="speed = 0.5";
+        }
+        if(rightwristy > 100 && rightwristy <= 200){
+            song.rate(1);
+            document.getElementById("speed").innerHTML="speed = 1";
+        }
+        if(rightwristy > 200 && rightwristy <= 300){
+            song.rate(1.5);
+            document.getElementById("speed").innerHTML="speed = 1.5";
+        }
+        if(rightwristy > 300 && rightwristy <= 400){
+            song.rate(2);
+            document.getElementById("speed").innerHTML="speed = 2";
+        }
+        if(rightwristy > 400 && rightwristy <= 500){
+            song.rate(2.5);
+            document.getElementById("speed").innerHTML="speed = 2.5";
+        }
+}
 }
 
 function play_song(){
@@ -43,6 +78,9 @@ function gotposes(results){
 
         console.log("left wrist x =" + leftwristx + " left wrist y = " + leftwristy)
         console.log("right wrist x =" + rightwristx + " right wrist y = " + rightwristy)
+
+        scoreleftwrist=results[0].pose.keypoints[9].score
+        scorerightwrist=results[0].pose.keypoints[10].score
     }
 
 }
